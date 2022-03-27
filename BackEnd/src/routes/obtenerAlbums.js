8b3d9339bf83d00d0e0e0ace5485cce448cfbf33
edwinlg17 <2247574310101@ingenieria.usac.edu.gt>
2022-03-27 17:59:43 -0600

    se agrego una funcion para obtener albums

---
+++ BackEnd/src/routes/obtenerAlbums.js
@@ -0,0 +1,19 @@
+const con = require('../../database/conection')//conexion bd
+
+async function obtenerAlbums(username) {
+    try {
+        const pool = await con;
+        const result = await pool.request()
+            .query(`select count(*) as cantidad from seminario1.usuario us
+            inner join seminario1.album al on us.idUser = al.idUser
+            where us.name = '${username}'`);
+
+        let recordset = result.recordset[0].cantidad;
+        return recordset;
+    } catch (err) {
+        console.log('ERROR' + err);
+        return -1;
+    }
+}
+
+module.exports = obtenerAlbums
